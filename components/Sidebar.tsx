86b31ed6b2730d420eb86a00cca627702958c6ae
TrueLifeDev <truelife1228@outlook.com>
2022-03-30 01:17:38 -0400

    update list

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -93,7 +93,37 @@ export default function Sidebar({ routes }) {
                           </div>
                         )
                       })}
+                    <div>
+                      <a href={`https://discord.gg/XpqkTbNcKM`} className="pl-2 flex text-gray hover:text-docblue text-lg py-2 transition ease-in-out duration-150 w-full font-Catamaran">
+                        <div className='w-full flex flex-row items-center justify-between'>
+                          <div>Discord</div>
+                          <svg x="0px" y="0px" viewBox="0 0 100 100" width="20" height="20" className="-mt-1 css-19vhmgv">
+                            <path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path>
+                            <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon>
+                          </svg>
+                        </div>
+                      </a>
+                      <a href={`https://t.me/hedgeplus_io`} className="pl-2 flex text-gray hover:text-docblue text-lg py-2 transition ease-in-out duration-150 w-full font-Catamaran">
+                        <div className='w-full flex flex-row items-center justify-between'>
+                          <div>Telegram</div>
+                          <svg x="0px" y="0px" viewBox="0 0 100 100" width="20" height="20" className="css-19vhmgv">
+                            <path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path>
+                            <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon>
+                          </svg>
+                        </div>
+                      </a>
+                      <a href={`https://twitter.com/hedgeplus_io`} className="pl-2 flex text-gray hover:text-docblue text-lg py-2 transition ease-in-out duration-150 w-full font-Catamaran">
+                        <div className='w-full flex flex-row items-center justify-between'>
+                          <div>Twitter</div>
+                          <svg x="0px" y="0px" viewBox="0 0 100 100" width="20" height="20" className="css-19vhmgv">
+                            <path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path>
+                            <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon>
+                          </svg>
+                        </div>
+                      </a>
+                    </div>
                   </div>
+
                 )
               }
               <nav className='sidebar__nav mt-8 flex-1 px-4 space-y-1'>
